9a1f4ea0495d0fcaadc31dfea6a669b28cc343d6
ilayasis <ilayasis2003@gmail.com>
2022-01-24 23:10:24 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,94 @@
+'use strict';
+
+// select elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
+let currentScore, activePlayer, playing, scores;
+//starting conditions
+const startGame = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+
+startGame();
+//rolling dice functionality
+
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    //1 נגריל מספר
+    const dice = Math.trunc(Math.random() * 6) + 1;
+
+    //2 נראה תמונה של הקוביה
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    //3 נבדוק אם התוצאה בקוביה 1, אם לא -> נוסיף את הערך לקארנט
+    if (dice !== 1) {
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // החלפה בין השחקנים
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    // תחילה נוסיף את הניקוד לשחקן האקטיבי
+    scores[activePlayer] += currentScore;
+    console.log(scores[activePlayer]);
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    // נבדוק אם השחקן הגיע ל100 נק לפחות
+    //אם כן נסיים את המשחק
+    if (scores[activePlayer] >= 20) {
+      playing = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      querySelector(`.player--${activePlayer}`).classList.remove(
+        'player--active'
+      );
+    } else {
+      //אם לא החלף שחקן
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener('click', startGame);
